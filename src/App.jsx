02828c8eb2e1629f65953137c09236319824072e
nnatroo <nugonatroshvili@gmail.com>
2023-06-29 02:24:55 +0400

    Added all products page

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ function App() {
             <Routes>
                 <Route path='/' element={<Home />}  />
                 <Route path='/products' element={<Products />}  />
+                <Route path='/products' element={<Products />}  />
                 <Route path='/contact' element={<Contact />}  />
             </Routes>
         </Router>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,12 @@ import classes from '../modules/Header.module.scss'
 import layout from '../modules/Layout.module.scss'
 import logo from '../assets/logo.jpg'
 import cart from '../assets/cart.jpg'
+import productsLogo from '../assets/products.png'
 import delivery from '../assets/delivery.svg'
 import discount from '../assets/discount.svg'
 import location from '../assets/location.svg'
+import {Link} from "react-router-dom";
+import React from "react";
 
 const Header = () => {
 
@@ -38,12 +41,21 @@ const Header = () => {
 		<section className={classes['header']}>
 			<div className={`${classes['container']} ${layout['container']}`}>
 				<div className={classes['logo']}>
-					<figure>
-						<img src={logo} alt="logo"/>
-						<h1>the jarba emporiom</h1>
-					</figure>
+					<Link to={'/'}>
+						<figure>
+							<img src={logo} alt="logo"/>
+							<h1>the jarba emporiom</h1>
+						</figure>
+					</Link>
 				</div>
 				<div className={classes['cart-wrapper']}>
+					<Link to={'/products'}>
+						<figure>
+							<img src={productsLogo} alt="cart"/>
+							<h2>All Products</h2>
+						</figure>
+					</Link>
+
 					<figure>
 						<img src={cart} alt="cart"/>
 						<h2>Cart</h2>
