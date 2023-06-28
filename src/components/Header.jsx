ce8acc52c1911587e6068f023429cc963f9ac382
nnatroo <nugonatroshvili@gmail.com>
2023-06-28 15:19:57 +0400

    Added header section

---
+++ src/components/Header.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import classes from '../modules/Header.module.scss'
+import logo from '../assets/logo.jpg'
+import cart from '../assets/cart.jpg'
+import delivery from '../assets/delivery.svg'
+import discount from '../assets/discount.svg'
+import location from '../assets/location.svg'
+
+const Header = (props) => {
+
+	return <>
+		<section className={classes['above-header']}>
+
+			<div className={classes['welcome-wrapper']}>
+				<span>Welcome to worldwide Megamart!</span>
+			</div>
+
+			<div className={classes['links-bar']} >
+				<ul>
+					<li>
+						<img src={delivery} alt=""/>
+						<span>Deliver to 423651</span>
+					</li>
+					<li>
+						<img src={location} alt=""/>
+						<span>Track your order</span>
+					</li>
+					<li>
+						<img src={discount} alt=""/>
+						<span>All Offers</span>
+					</li>
+				</ul>
+			</div>
+		</section>
+
+		<section className={classes['header']}>
+			<div className={classes['logo']}>
+				<figure>
+					<img src={logo} alt="logo"/>
+					<h1>the jarba emporiom</h1>
+				</figure>
+			</div>
+			<div className={classes['cart-wrapper']}>
+				<figure>
+					<img src={cart} alt="cart"/>
+					<h2>Cart</h2>
+				</figure>
+			</div>
+		</section>
+	</>
+};
+
+export default Header;
